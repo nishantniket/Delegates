ecaaad40ceaffba1bc3502c542d1df4118e88de8
unknown <nishantniket@gmail.com>
2019-02-01 18:01:38 +0530

    Delegates Basic wrapped up

---
+++ Delegates/PhotoProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates
+{
+    public class PhotoProcessor
+    {
+        // This is a custom delegate
+        //public delegate void PhotoFilterHandler(Photo photo);
+       // public void Process(string path, PhotoFilterHandler filterHandler)
+        public void Process(string path, Action<Photo> filterHandler)
+        {
+            var photo = Photo.Load(path);
+            filterHandler(photo);
+            //var filters = new PhotoFilters();
+            //filters.ApplyBrightness(photo);
+            //filters.ApplyContrast(photo);
+            //filters.Resize(photo);
+            photo.Save();
+
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor= new PhotoProcessor();
+            var filters = new PhotoFilters();
+            //  PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+            Action<Photo> filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedFilter;
+            processor.Process("photo.jpg",filterHandler);
+        }
+
+        static void RemoveRedFilter(Photo photo)
+        {
+            Console.WriteLine("Apply RemoveRed");
+        }
+    }
+}
